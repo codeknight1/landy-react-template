e61617bdc6f64b2afbed439f514cd477b2384b31
Codeknight <71535757+codeknight1@users.noreply.github.com>
2025-02-18 17:10:18 +0100

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -59,7 +59,7 @@ const Header = ({ t }: { t: TFunction }) => {
       <Container>
         <Row justify="space-between">
           <LogoContainer to="/" aria-label="homepage">
-            <SvgIcon src="logo.svg" width="201px" height="64px" />
+            <SvgIcon src="logo.svg" width="301px" height="64px" />
           </LogoContainer>
           <NotHidden>
             <MenuItem />
